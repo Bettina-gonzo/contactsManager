0b8ed7578f76da2fddddeaf8c9057e3b9e01c744
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-13 16:14:30 -0500

    comments added for constructors and getters

--- src/Contacts.java
+++ src/Contacts.java
@@ -6,17 +6,17 @@ import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class Contacts { //
-    private String fullName;
-    private String phoneNumber;
+    private String fullName; //variable for full name
+    private String phoneNumber; //variable for phone number
 
-    public Contacts(String fullName, String phoneNumber) {
+    public Contacts(String fullName, String phoneNumber) { //constructor for contacts
         this.fullName = fullName;
         this.phoneNumber = phoneNumber;
     }
 
     public String getFullName() {
         return fullName;
-    }
+    } //getter for first and last name
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
@@ -24,7 +24,7 @@ public class Contacts { //
 
     public String getPhoneNumber() {
         return phoneNumber;
-    }
+    } //getter for phone number
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
